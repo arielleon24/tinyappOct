7aad3df18834db4c7f2ba412297cead107be816b
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2020-11-10 00:18:19 +0000

    set Ejs as viewing engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// sets EJS as the viewing engine
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -13,6 +16,10 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) =>{
   res.json(urlDatabase)
+});
+
+app.get("/hello",(req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
